c056fb0943acc37ee459b53608554d751db6e8e3
ayan4m1 <andrew@bulletlogic.com>
2023-12-19 21:22:52 -0500

    adjust wording on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,11 +6,11 @@ import Layout from 'components/layout';
 export default function NotFoundPage() {
   return (
     <Layout>
-      <SEO title="Not Found" />
+      <SEO title="Page Not Found" />
       <Container>
         <Row>
           <Col md="12">
-            <h1>Not Found</h1>
+            <h1>Page Not Found</h1>
             <p>Sorry, this URL is not valid.</p>
           </Col>
         </Row>
