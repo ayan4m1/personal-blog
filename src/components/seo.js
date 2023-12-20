72f4537f5c701d02f18bfeaa3c3d5bb0071a38ba
ayan4m1 <andrew@bulletlogic.com>
2023-12-19 21:23:15 -0500

    remove twitter from SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -45,22 +45,6 @@ export default function SEO({
         {
           property: `og:type`,
           content: `website`
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author
-        },
-        {
-          name: `twitter:title`,
-          content: title
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription
         }
       ].concat(meta)}
     />
