0cfbce64c71e83606db6075535715e900bba7776
ayan4m1 <andrew@bulletlogic.com>
2023-12-24 15:18:53 -0500

    add snow for christmas

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,20 @@
 import PropTypes from 'prop-types';
 import { Fragment } from 'react';
+import Snowfall from 'react-snowfall';
 
 import Header from 'components/header';
 import useDarkMode from 'hooks/useDarkMode';
+import { getDate, getMonth } from 'date-fns';
 
 export default function Layout({ children }) {
   useDarkMode();
 
+  const isChristmas =
+    getMonth(Date.now()) === 11 && [24, 25, 26].includes(getDate(Date.now()));
+
   return (
     <Fragment>
+      {isChristmas && <Snowfall />}
       <Header />
       <main className="mt-3 mb-2">{children}</main>
     </Fragment>
