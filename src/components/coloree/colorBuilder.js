1909ea704af373c2e88ecd4c1a57b91e18467aaa
ayan4m1 <andrew@bulletlogic.com>
2024-01-28 20:47:08 -0500

    wip image color extraction

--- src/components/coloree/colorBuilder.js
+++ src/components/coloree/colorBuilder.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { SketchPicker } from 'react-color';
-import { useState, useCallback, useMemo, Fragment } from 'react';
+import { useState, useCallback, useMemo, useRef, Fragment } from 'react';
 import {
   faDice,
   faPlusCircle,
@@ -17,14 +17,17 @@ import {
   Row,
   Col
 } from 'react-bootstrap';
+import palette from 'get-rgba-palette';
 
 import {
   createRandomPuzzle,
   createRandomColor,
   getRemainingPct
 } from 'utils/coloree';
+import { rgb } from 'd3-color';
 
 export default function ColorBuilder({ colors, onSliceAdd, onSoloPlayClick }) {
+  const canvasRef = useRef();
   const maxSlicePercentage = useMemo(() => getRemainingPct(colors), [colors]);
   const [difficulty, setDifficulty] = useState(1);
   const [selectedColor, setSelectedColor] = useState(createRandomColor());
@@ -58,95 +61,144 @@ export default function ColorBuilder({ colors, onSliceAdd, onSoloPlayClick }) {
       setDifficulty(Math.min(6, Math.max(1, parseInt(e.target.value, 10)))),
     []
   );
+  const handleFileChange = useCallback(
+    async (event) => {
+      const [file] = event.target.files;
+      const bitmap = await createImageBitmap(file);
+
+      if (!canvasRef.current) {
+        return;
+      }
+
+      canvasRef.current.attributes['width'] = bitmap.width;
+      canvasRef.current.attributes['height'] = bitmap.height;
+
+      const ctx = canvasRef.current.getContext('2d');
+
+      ctx.drawImage(bitmap, 0, 0);
+
+      const { data } = ctx.getImageData(0, 0, bitmap.width, bitmap.height);
+
+      const colorPalette = palette.bins(data, 5, 10, (pixels, index) => {
+        const averageValue =
+          pixels
+            .slice(index, index + 2)
+            .reduce((prev, curr) => prev + curr, 0) / 3;
+
+        return averageValue > 80;
+      });
+
+      for (const { color, amount } of colorPalette) {
+        const colorHex = rgb(...color).formatHex();
+
+        onSliceAdd(colorHex, amount);
+      }
+    },
+    [canvasRef, onSliceAdd]
+  );
 
   return (
     <Fragment>
-      <Card body className="mb-3">
-        <Card.Title>Solo Play</Card.Title>
-        <Alert variant="info" className="my-2">
-          Play a game on your own with a randomly generated puzzle!
-        </Alert>
-        <div className="text-end">
-          <Button variant="success" onClick={onSoloPlayClick}>
-            <FontAwesomeIcon icon={faWandMagicSparkles} /> Start Game
-          </Button>
-        </div>
-      </Card>
-      <Card body className="mb-3">
-        <Card.Title>Generate a Puzzle</Card.Title>
-        <Alert variant="info" className="my-2">
-          Create a randomly generated puzzle to share with your friends!
-        </Alert>
-        <Form>
-          <Form.Group>
-            <Form.Label>Difficulty (1 - 6)</Form.Label>
-            <Form.Control
-              type="number"
-              min={1}
-              max={6}
-              step={1}
-              value={difficulty}
-              onChange={handleDifficultyChange}
-            />
-          </Form.Group>
-          <Form.Group className="text-end">
-            <Button
-              className="mt-2"
-              variant="success"
-              onClick={handlePuzzleRandomize}
-            >
-              <FontAwesomeIcon icon={faDice} /> Generate
+      <Col xs={12} md={6}>
+        <Card body className="mb-3">
+          <Card.Title>Solo Play</Card.Title>
+          <Alert variant="info" className="my-2">
+            Play a game on your own with a randomly generated puzzle!
+          </Alert>
+          <div className="text-end">
+            <Button variant="success" onClick={onSoloPlayClick}>
+              <FontAwesomeIcon icon={faWandMagicSparkles} /> Start Game
             </Button>
-          </Form.Group>
-        </Form>
-      </Card>
-      <Card body className="mb-3">
-        <Card.Title>Create a Puzzle</Card.Title>
-        <Alert variant="info" className="my-2">
-          Use the color picker and percentage inputs below to add colors up to
-          100%. Then, share your puzzle with friends and see if you can stump
-          them!
-        </Alert>
-        <Form>
-          <Row>
-            <Col xs={12} md={6} className="mb-2">
-              <Form.Label>Color</Form.Label>
-              <SketchPicker
-                color={selectedColor}
-                onChange={handleSliceColorChange}
-                presetColors={[]}
-                disableAlpha
+          </div>
+        </Card>
+      </Col>
+      <Col xs={12} md={6}>
+        <Card body className="mb-3">
+          <Card.Title>Generate a Puzzle</Card.Title>
+          <Alert variant="info" className="my-2">
+            Create a randomly generated puzzle to share with your friends!
+          </Alert>
+          <Form>
+            <Form.Group>
+              <Form.Label>Difficulty (1 - 6)</Form.Label>
+              <Form.Control
+                type="number"
+                min={1}
+                max={6}
+                step={1}
+                value={difficulty}
+                onChange={handleDifficultyChange}
               />
-            </Col>
-            <Col xs={12} md={6} className="mb-2">
-              <Form.Label>Percentage</Form.Label>
-              <InputGroup>
-                <Form.Control
-                  type="number"
-                  min={0}
-                  max={maxSlicePercentage}
-                  step={1}
-                  onChange={handleSlicePctChange}
-                  value={slicePercentage}
+            </Form.Group>
+            <Form.Group className="text-end">
+              <Button
+                className="mt-2"
+                variant="success"
+                onClick={handlePuzzleRandomize}
+              >
+                <FontAwesomeIcon icon={faDice} /> Generate
+              </Button>
+            </Form.Group>
+          </Form>
+        </Card>
+        <Card body className="mb-3">
+          <Card.Title>Create a Puzzle By Hand</Card.Title>
+          <Alert variant="info" className="my-2">
+            Use the color picker and percentage inputs below to add colors up to
+            100%. Then, share your puzzle with friends and see if you can stump
+            them!
+          </Alert>
+          <Form>
+            <Row>
+              <Col xs={12} md={6} className="mb-2">
+                <Form.Label>Color</Form.Label>
+                <SketchPicker
+                  color={selectedColor}
+                  onChange={handleSliceColorChange}
+                  presetColors={[]}
+                  disableAlpha
                 />
-                <InputGroup.Text>
-                  of {Math.round(maxSlicePercentage)}%
-                </InputGroup.Text>
-              </InputGroup>
-            </Col>
-          </Row>
-          <Form.Group className="text-end">
-            <ButtonGroup>
-              <Button variant="success" onClick={handleSliceAdd}>
-                <FontAwesomeIcon icon={faPlusCircle} /> Add Color
-              </Button>
-              <Button variant="info" onClick={handleSliceRandomize}>
-                <FontAwesomeIcon icon={faDice} /> Randomize Color
-              </Button>
-            </ButtonGroup>
-          </Form.Group>
-        </Form>
-      </Card>
+              </Col>
+              <Col xs={12} md={6} className="mb-2">
+                <Form.Label>Percentage</Form.Label>
+                <InputGroup>
+                  <Form.Control
+                    type="number"
+                    min={0}
+                    max={maxSlicePercentage}
+                    step={1}
+                    onChange={handleSlicePctChange}
+                    value={slicePercentage}
+                  />
+                  <InputGroup.Text>
+                    of {Math.round(maxSlicePercentage)}%
+                  </InputGroup.Text>
+                </InputGroup>
+              </Col>
+            </Row>
+            <Form.Group className="text-end">
+              <ButtonGroup>
+                <Button variant="success" onClick={handleSliceAdd}>
+                  <FontAwesomeIcon icon={faPlusCircle} /> Add Color
+                </Button>
+                <Button variant="info" onClick={handleSliceRandomize}>
+                  <FontAwesomeIcon icon={faDice} /> Randomize Color
+                </Button>
+              </ButtonGroup>
+            </Form.Group>
+          </Form>
+        </Card>
+        <Card body className="mb-2">
+          <Card.Title>Create a Puzzle from an Image</Card.Title>
+          <Form>
+            <Form.Group>
+              <Form.Label>Image File</Form.Label>
+              <Form.Control type="file" onChange={handleFileChange} />
+            </Form.Group>
+            <canvas ref={canvasRef} style={{ display: 'none' }} />
+          </Form>
+        </Card>
+      </Col>
     </Fragment>
   );
 }
